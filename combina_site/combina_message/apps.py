22527a100d51de9512d82de57a8f075e9f634b1a
hcy0157 <harnm0157@gmail.com>
2024-05-06 17:01:36 +0900

    장고 앱 생성

---
+++ combina_site/combina_message/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CombinaMessageConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'combina_message'
---
+++ combina_site/combina_site/urls.py
@@ -0,0 +1,25 @@
+"""
+URL configuration for combina_site project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('combina_message/', include('combina_message.urls')),
+    path('combina_post/', include('combina_post.urls')),
+    path('combina_search/', include('combina_search.urls')),
+    path('admin/', admin.site.urls),
+]
