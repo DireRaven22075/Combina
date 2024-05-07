c5b618cd258cab68fc4f746b6add59e917a3659a
Han Yoon Soo <hanhs9803753@gmail.com>
2024-05-07 14:56:34 +0900

    Clearn Up Code

--- Script/Home.js
+++ Script/Home.js
@@ -1,3 +1,25 @@
 window.onload = function() {
     scrollTo(0, 0);
 };
+function search(type) {
+    var home = document.getElementById("Home");
+    var children = Array.from(home.children);
+    children.forEach(function(child) {
+        if (child.classList.contains(type)) {
+            child.style.display = "block";
+        } else {
+            if (child.classList.contains())
+            child.style.display = "none";
+        }
+    });
+}
+function unselect() {
+    var home = document.getElementById("Home");
+    var children = Array.from(home.children);
+    children.forEach(function(child) {
+        child.style.display = "block";
+        console.log(child.classList);
+    });
+    console.log("Unselected");
+
+};
--- Script/static.js
+++ Script/static.js
@@ -3,6 +3,27 @@ function goto(location = "home.html") {
     window.location.href = location;
     return;
 }
+var selectedTag = [];
+function search(type) {
+    var docs = document.title.split(" : ")[1];
+    var home = document.getElementById(docs);
+    var children = Array.from(home.children);
+    var title = document.getElementById("Title");
+    if (type == "All") {
+        children.forEach((child) => child.style.display = "block");
+        switch (docs) {
+            default: case "Home": title.innerHTML = "Home"; return;
+            case "Index": title.innerHTML = "Combina"; return;
+            case "Search": title.innerHTML = "All"; return;
+        }
+    }
+    else {
+        children.forEach((child) => child.style.display = child.classList.contains(type) ? "block" : "none");
+        title.innerHTML = type;
+        return;
+    }
+}
+
 window.onload += function() {
     const lower = document.getElementById("lower");
 }
