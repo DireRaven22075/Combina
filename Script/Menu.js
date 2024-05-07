298f26ebe00d6a4880bdf2945ee4cb43a63760de
Han Yoon Soo <hanhs9803753@gmail.com>
2024-05-07 16:58:20 +0900

    Update Menu.html

---
+++ Script/Menu.js
@@ -0,0 +1,31 @@
+function login(name) {
+    temp(name);
+    return;
+    switch (name) {
+        case "Facebook":
+            temp
+            return;
+    }
+}
+function showlist() {
+    const body = document.getElementById("AccountList");
+    const text = document.getElementsByClassName("add")[0].children[0];
+    const image = document.getElementsByClassName("add")[0].children[0];
+    if (body.style.display == "block") {
+        body.style.display = "none";
+        text.innerHTML = "창 닫기";
+        image.style.display = "none";
+        return;
+    } else {
+        body.style.display = "block";
+        text.innerHTML = "계정 추가";
+        image.style.display = "block";
+        return;
+    }
+    body.style.display = (body.style.display == "block") ? "none" : "block";
+}
+function temp(name) {
+    const body = document.getElementById(name);
+    body.style.display = "block";
+
+}
