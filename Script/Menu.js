45a2743b0c124e8325cd4b8d343910f3b4363311
Han Yoon Soo <hanhs9803753@gmail.com>
2024-05-21 20:46:37 +0900

    Update to Django

--- Script/Menu.js
+++ Script/Menu.js
@@ -1,26 +1,7 @@
-function login(name) {
-    temp(name);
-    return;
+function closePopUp() {
+    const id = document.getElementById("PopUp");
 }
-function showlist() {
-    const body = document.getElementById("AccountList");
-    const text = document.getElementsByClassName("add")[0].children[0];
-    const image = document.getElementsByClassName("add")[0].children[0];
-    if (body.style.display == "block") {
-        body.style.display = "none";
-        text.innerHTML = "창 닫기";
-        image.style.display = "none";
-        return;
-    } else {
-        body.style.display = "block";
-        text.innerHTML = "계정 추가";
-        image.style.display = "block";
-        return;
-    }
-    body.style.display = (body.style.display == "block") ? "none" : "block";
-}
-function temp(name) {
-    const body = document.getElementById(name);
-    body.style.display = "block";
-
-}
+window.onload += () => {
+    const id = document.getElementById("PopUp");
+    id.style.display = "none";
+};
