9f212e0e578ffe045a01b52ea4354f18c9112294
Han Yoon Soo <hanhs9803753@gmail.com>
2024-05-31 19:48:12 +0900

    Chore : Move static folder to root
    Move static folder for django to BASE_DIR

---
+++ static/js/Menu.js
@@ -0,0 +1,32 @@
+function closePopUp() {
+    const id = document.getElementById("PopUp");
+    id.style.display = "none";
+}
+document.addEventListener("click", function (e) {
+    const id = document.getElementById("PopUp");
+    if (id.style.display === "block" && e.target === id) {
+        id.style.display = "none";
+    }
+});
+window.onload += () => {
+    const id = document.getElementById("PopUp");
+    id.style.display = "none";
+};
+function disconnect(json) {
+    const id = document.getElementById("PopUp");
+    id.style.display = "block";
+    const text = document.getElementById("PopUp_Text");
+    text.innerHTML = json["name"] + "  [" + json["platform"] + "]";
+    const data = document.getElementById("PopUp_Data");
+    data.value = JSON.stringify(json);
+}
+function connect(json) {
+
+}
+function post(event) {
+    if (event.target.tagName !== 'FORM') {
+        event.target.closest('form').submit();
+    } else {
+        event.target.submit();
+    }
+}
