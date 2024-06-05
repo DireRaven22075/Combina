be9c09815de20ef625ddaffce1aa07101f8d6f27
Han Yoon Soo <hanhs9803753@gmail.com>
2024-06-05 18:28:29 +0900

    Chore : Change Models in page application
    Change Model to use multiple platform

--- page/models.py
+++ page/models.py
@@ -8,21 +8,14 @@ class AccountDB(models.Model):
     connected = models.BooleanField(default=False, null=False)
 
 class ContentDB(models.Model):
-    platform = models.CharField(max_length=30)
-    time = models.DateTimeField()
-    name = models.CharField(max_length=100, null=True)
-    text = models.TextField()
-    file = models.IntegerField()
-    author_id = models.BigIntegerField(null=True)  # DiscordMessage의 author_id 추가
-    image_url = models.URLField(blank=True, null=True)  # DiscordMessage의 image_url 추가
-    profile_image_url = models.URLField(blank=True, null=True)  # DiscordMessage의 profile_image_url 추가
+    platform = models.CharField(max_length=30) #Discord == discord // Discord
+    userID = models.CharField(max_length=100) #Discord : user_id // Everytime: username
+    text = models.TextField() #Discord : content // Everytime: title + text
+    image_url = models.BigIntegerField()
+    userIcon = models.URLField()
+    vote = models.BigIntegerField()
 
 class FileDB(models.Model):
     uid = models.IntegerField()
-    path = models.FilePathField()
-    file = models.FileField()
-
-class ChatDB(models.Model):
-    owner = models.IntegerField()
-    text = models.TextField()
-    time = models.DateTimeField()
+    url = models.URLField()
+# Modified 20240.06.05
