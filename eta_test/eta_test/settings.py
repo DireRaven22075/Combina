21f219fce2f99d57e9cddf1a49da77eb7866c7aa
hcy0157 <harnm0157@gmail.com>
2024-06-05 11:20:44 +0900

    test everytime

--- eta_test/eta_test/settings.py
+++ eta_test/eta_test/settings.py
@@ -31,6 +31,12 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #테스트 models
+    'page',
+    'account',
+    'post',
+
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- eta_test/page/views.py
+++ eta_test/page/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from .utils import driver_set
 # Create your views here.
+
+
+
+user = driver_set()
+
+
+
+def home(request):
+    
+    return render(request, 'page/home.html')
+
+def search(request):
+    if request.method == 'POST':
+        search = request.POST.get('search')
+        print(f"search content : {search}")
+
+    return render(request, 'page/home.html')
