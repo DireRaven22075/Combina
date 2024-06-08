5cb0dc56485d022051293d8a21912b83658123a8
AEROKnight0210 <19562627+jinhyung2003@users.noreply.github.com>
2024-06-09 04:09:47 +0900

    Update navigation.js

--- static/js/ui-updates/navigation.js
+++ static/js/ui-updates/navigation.js
@@ -10,7 +10,7 @@ function navigation(platform) {
         case "Instagram": target.style.backgroundImage = 'linear-gradient(to right, rgb(255, 0, 169), rgb(255, 139, 0))'; break;
         case "X": target.style.background = "#101010"; element.style.color = "#FFFFFF"; break;
         case "Discord": target.style.background = "#6900FF"; break;
-        case "Reddit": target.style.background = "#FF4300"; break;
+        case "Reddit": target.style.background = "#FF4500"; break;
         case "Everytime": target.style.background = "#303030"; break;
         case "Youtube": target.style.background = "#FF1A00"; break;
     }
