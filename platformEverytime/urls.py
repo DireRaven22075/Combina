82b31b37ab35daf0df8b597ae2aae07b1cbcd497
hcy0157 <harnm0157@gmail.com>
2024-06-13 03:44:15 +0900

    explain in platformEverytime/urls.py

--- platformEverytime/urls.py
+++ platformEverytime/urls.py
@@ -3,7 +3,10 @@ from django.urls import path
 from .views import Everytime
 from asgiref.sync import async_to_sync
 
-# POST["page"] 값 init 인지 account인지 구별 및 처리
+# 에브리타임은 크롤링을 연속적으로 하게 되면, 쿠키에 기록이 남고 로그인을 막음
+# 환경이 바뀌었을 때 얼마까지 되는지는 모르겠음.
+# driver의 설정은 account.py에 있음 account.py의 64번째 줄 쿠키 일일일 지우려고 길게함. 새 환경에서는 줄여도 괜찮을 거임
+# post.py의 79,80 번째줄은 업로드 하고 싶을 때 주석 해제하면 됨.
 
 urlpatterns = [
     path('connect/', Everytime.login_page, name='login'), #로그인 페이지 랜더링
