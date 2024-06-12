49014335f6e70157d490c76ab8f5294e21c7a630
AEROKnight0210 <19562627+jinhyung2003@users.noreply.github.com>
2024-06-12 17:37:08 +0900

    Feat : Create Application
    Create Applicaion for platforms
    Instagram, Facebook, Youtube, Reddit

--- platformReddit/friends.py
+++
@@ -1,9 +0,0 @@
-def get_friends_list():
-    # Placeholder for friends list as Reddit API does not support this feature
-    # You can replace this with actual implementation if the API supports it in the future
-    friends = [
-        {"name": "friend1", "profile_url": "https://www.reddit.com/user/friend1"},
-        {"name": "friend2", "profile_url": "https://www.reddit.com/user/friend2"},
-        {"name": "friend3", "profile_url": "https://www.reddit.com/user/friend3"}
-    ]
-    return friends
