e52ba0be06623758a59350890f0bfbb0841212e2
hcy0157 <harnm0157@gmail.com>
2024-06-13 08:34:59 +0900

    making clear in platformEverytime

--- platformEverytime/non_post/urls.py
+++
@@ -1,8 +0,0 @@
-from . import views
-from django.urls import include, path
-from .views import *
-
-urlpatterns = [
-    path('', views.post, name="first page to post"),
-    path('post_info/', views.post, name="post_info"),
-]
--- platformEverytime/non_search/urls.py
+++
@@ -1,8 +0,0 @@
-from . import views
-from django.urls import include, path
-from .views import *
-
-urlpatterns = [
-    path('home/', views.home, name="home"),
-    path('search/', views.search, name="search_posts"),
-]
