2161fd18fae7ae0e9e549ff0283e8b46b3deed40
AEROKnight0210 <19562627+jinhyung2003@users.noreply.github.com>
2024-06-14 12:20:39 +0900

    .

--- platformYoutube/urls.py
+++ platformYoutube/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from .views import YouTubeView
 
 urlpatterns = [
-    path('connect/', YouTubeView.Connect, name='connect'),
+    path('connect/', YouTubeView.Connect, name='login'),
     path('callback/', YouTubeView.ConnectCallback, name='callback'),
-    path('disconnect/', YouTubeView.Disconnect, name='disconnect'),
-    path('get-content/', YouTubeView.GetContent, name='get_content'),
+    path('disconnect/', YouTubeView.Disconnect, name='logout'),
+    path('get-content/', YouTubeView.GetContent, name='recommended'),
+    path('clear-contents/', YouTubeView.ClearContent, name='clear_contents'),  # Add this line
 ]
