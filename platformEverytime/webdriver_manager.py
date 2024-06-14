cc36cb6e0d0beaa2328f17502f86a65b80d1c894
hcy0157 <harnm0157@gmail.com>
2024-06-14 19:40:25 +0900

    make clear platformEvyerimte

--- platformEverytime/webdriver_manager.py
+++ platformEverytime/webdriver_manager.py
@@ -21,18 +21,11 @@ class WebDriverManager:
     def _create_default_options(self):
         options = Options()
         options = webdriver.ChromeOptions()
-        #options.add_argument("--headless") # 브라우저 띄우지 않음
         options.add_argument("--log-level=3")
         options.add_argument("--remote-debugging-port=9222")
         options.add_argument('--disable-blink-features=AutomationControlled')
-        #options.add_argument("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36")
-        #options.add_argument("--profile-default-content-settings.cookies=1") # 쿠키 허용
-        #subprocess.Popen(r'C:\\Program Files\\Google\\Chrome\Application\\chrome.exe --remote-debugging-port=9222 --user-data-dir="C:\\chromeCookie"')
-        #options.add_experimental_option("debuggerAddress", "127.0.0.1:8000") # 디버거 주소
-        #options.add_experimental_option("prefs", prefs) # 이미지, CSS, 자바스크립트 로드 설정
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
-        #driver =  webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
         return options
 
     def get_driver(self, custom_options=None):
@@ -47,7 +40,6 @@ class WebDriverManager:
                 try:
                     self.driver = webdriver.Chrome(options=options)
                 except SessionNotCreatedException:
-                    print("ChromeDriver is not compatible with Chrome Browser")
                     return None
             return self.driver
 
@@ -65,7 +57,6 @@ class WebDriverManager:
                     self.driver.current_url
                     return True
                 except Exception as e:
-                    print(f"WebDriver is not stable: {str(e)}")
                     return False
             else:
                 return False
