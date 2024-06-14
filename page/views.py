f320908ee8f480275102e2282991190d24a6ad6a
Han Yoon Soo <hanhs9803753@gmail.com>
2024-06-15 02:26:59 +0900

    Feat : Re-design models

--- page/views.py
+++ page/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
+def platforms():
+    return ["Facebook", "Reddit", "Youtube", "Everytime", "imgur"]
 def parameters():
     data = {}
-    data['platforms'] = [
-        "Facebook", "Instagram", "Discord",
-        "Reddit", "Everytime", "Youtube"
-    ]
+    data['platforms'] = platforms()
     data['contents'] = ContentDB.objects.all()
     data['accounts'] = AccountDB.objects.all()
     data['files'] = FileDB.objects.all()
@@ -20,28 +19,10 @@ def isOkay():
 class PageView:
     def Welcome(request):
         if (AccountDB.objects.all().count() == 0):
-            AccountDB.objects.create(platform='Facebook', connected=False)
-            AccountDB.objects.create(platform='Instagram', connected=False)
-            AccountDB.objects.create(platform='Discord', connected=False)
-            AccountDB.objects.create(platform='Reddit', connected=False)
-            AccountDB.objects.create(platform='Everytime', connected=False)
-            AccountDB.objects.create(platform='Youtube', connected=False)
-            AccountDB.objects.filter(platform='Youtube').first().token = '{"installed":{"client_id":"1093025684898-1kdj5micd00haaeo3g0kr9n9fep49fev.apps.googleusercontent.com","project_id":"combina-youtube","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://oauth2.googleapis.com/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"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","redirect_uris":["http://localhost"]}}'
-            AccountDB.objects.filter(platform='Youtube').first().save()
-        
-        if (request.session.get('visited') == None):
-            request.session['visited'] = True
-            return render(request, 'page/00_welcome1.html', {'connected': True})
-        try:
-            redirect('/discord/get-content')
-            redirect('/reddit/get-content')
-            redirect('/youtube/get-content')
-            redirect('/everytime/get-content')
-            redirect('/facebook/get-content')
-            redirect('/instagram/get-content')
-        except:
-            pass
-
+            for platform in platforms():
+                account = AccountDB(connected=False)
+                account.platform = platform
+                account.save()
         return render(request, 'page/00_welcome1.html', parameters())
     def Welcome2(request):
         return render(request, 'page/00_welcome2.html', parameters())
--- server/views.py
+++ server/views.py
@@ -3,26 +3,24 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
 from django.middleware.csrf import get_token
 from page.models import *
-from page.views import *
+from page.views import platforms
 
 class ServerView:
     def Disconnect(request):
-        platforms = parameters()['platforms']
         cookies = {'csrftoken': get_token(request)}
         headers = {
                 'Content-Type': 'application/json',
                 'csrfmiddlewaretoken': get_token(request),  # 'X-CSRFToken': 'token
                 'X-CSRFToken': get_token(request)
         }
-        for platform in platforms:
+        for platform in platforms():
             url = f'http://127.0.0.1:8000/{platform}/disconnect/'
-            response = requests.post(url, cookies=cookies, headers=headers)
+            requests.post(url, cookies=cookies, headers=headers)
         return redirect('http://127.0.0.1:8000/accounts', cookies=cookies, headers=headers)
     
     def GetContent(request):
         ContentDB.objects.all().delete()
         FileDB.objects.all().delete()
-        platforms = parameters()['platforms']
         cookies = {
                 'csrftoken': get_token(request)
         }
@@ -35,7 +33,7 @@ class ServerView:
             if (account.connected == False):
                 continue
             url = f'http://127.0.0.1:8000/{account.platform}/get-content/'
-            response = requests.post(url, cookies=cookies, headers=headers)
+            requests.post(url, cookies=cookies, headers=headers)
         return redirect('http://127.0.0.1:8000/home/', cookies=cookies)
     
     def Post(request):
@@ -51,11 +49,9 @@ class ServerView:
                 'csrfmiddlewaretoken': get_token(request),  # 'X-CSRFToken': 'token
                 'X-CSRFToken': get_token(request)
             }
-            
-            platforms = parameters()['platforms']
             base_url = "http://localhost:8000"  # 기본 URL 설정
 
-            for platform in platforms:
+            for platform in platforms():
                 if request.POST.get(platform):
                     url = f'{base_url}/{platform}/post/'
                     try:
