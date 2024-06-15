c01588a24f9d4577d66ef116d51dbdca0a84cb30
AEROKnight0210 <19562627+jinhyung2003@users.noreply.github.com>
2024-06-15 15:39:31 +0900

    Update settings.py

--- combina/settings.py
+++ combina/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     #"platformDiscord",
     #"platformReddit",
     "platformEverytime",
-    #"platformYoutube",
+    "platformYoutube",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
